2e7bd57468da0c16436e3d6ad4f609e8f1221a27
Jack Mocherman <mochermanjack@gmail.com>
2016-10-02 10:56:25 -0500

    composed three components to output into bug list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+'use strict';
+
+const BugFilter = React.createClass({
+	render: function() {
+		return (
+			<div>A way to filter bugs</div>
+		)
+	}
+})
+
+const BugTable = React.createClass({
+	render: function() {
+		return(
+			<div>Will be a table bugs</div>
+		)
+	}
+})
+
+const BugAdd = React.createClass({
+	render: function() {
+		return(
+			<div>Will be a form to add bugs</div>
+		)
+	}
+})
+
+const BugList = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<h1>Bug Tracker</h1>
+				<BugFilter />
+				<hr />
+				<BugTable />
+				<hr />
+				<BugAdd />
+			</div>
+		)
+	}
+})
+
 ReactDOM
 	.render(
-    <h1>Hello, World!</h1>,
-    document.getElementById('example')
+    <BugList />,
+    document.getElementById('main')
 	)
--- static/App.js
+++ static/App.js
@@ -1,5 +1,60 @@
-ReactDOM.render(React.createElement(
-   'h1',
-   null,
-   'Hello, World!'
-), document.getElementById('example'));
+'use strict';
+
+const BugFilter = React.createClass({
+	displayName: 'BugFilter',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			null,
+			'A way to filter bugs'
+		);
+	}
+});
+
+const BugTable = React.createClass({
+	displayName: 'BugTable',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			null,
+			'Will be a table bugs'
+		);
+	}
+});
+
+const BugAdd = React.createClass({
+	displayName: 'BugAdd',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			null,
+			'Will be a form to add bugs'
+		);
+	}
+});
+
+const BugList = React.createClass({
+	displayName: 'BugList',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'h1',
+				null,
+				'Bug Tracker'
+			),
+			React.createElement(BugFilter, null),
+			React.createElement('hr', null),
+			React.createElement(BugTable, null),
+			React.createElement('hr', null),
+			React.createElement(BugAdd, null)
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(BugList, null), document.getElementById('main'));
