2491e5d7fca4bf4d27b5109d8cb07242b859849a
Jack Mocherman <mochermanjack@gmail.com>
2016-10-03 14:02:53 -0500

    replaced bundle task with watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 const gulp = require('gulp')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+const watchify = require('watchify')
 
-gulp.task('bundle', () => {
-	return browserify('src/App.js')
-		.transform('babelify', {presets: 'react'})
-		.bundle()
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('static/'))
+gulp.task('watch', () => {
+	const b = browserify({
+		entries: ['src/App.js'],
+		cache: {},
+		packageCache: {},
+		plugin: ['watchify']
+	})
+
+	b.on('update', makeBundle)
+
+	function makeBundle() {
+		b
+			.transform('babelify', {presets: 'react'})
+			.bundle()
+			.pipe(source('bundle.js'))
+			.pipe(gulp.dest('static/'))
+	}
+	makeBundle()
+	return b
 })
