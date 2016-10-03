67179aef128376ea0c9d412fa6d0bfe972ce3596
Jack Mocherman <mochermanjack@gmail.com>
2016-10-03 13:54:19 -0500

    required react, reactDOM and jquery from node_modules instead of using cdns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const React = require('react')
+const ReactDOM = require('react-dom')
+const $ = require('jquery')
+
 const BugFilter = React.createClass({
 	render: function() {
 		return(
