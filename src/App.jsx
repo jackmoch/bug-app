3f7e3d0f1ad1e24eab1e2de1a05779375a8f4ea6
Jack Mocherman <mochermanjack@gmail.com>
2016-10-04 14:01:04 -0500

    added bug/id route to Router

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const Redirect = require('react-router').Redirect;
 const hashHistory = require('react-router').hashHistory
 
 const BugList = require('./BugList');
+const BugEdit = require('./BugEdit')
 
 const NoMatch = React.createClass({
   render: function() {
@@ -21,6 +22,7 @@ ReactDOM.render(
   (
     <Router history={hashHistory}>
       <Route path="/bugs" component={BugList} />
+      <Route path="/bugs/:id" component={BugEdit} />
       <Redirect from="/" to="/bugs" />
       <Route path="*" component={NoMatch} />
     </Router>
