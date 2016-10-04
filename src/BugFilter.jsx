9b9cffeb15049acbf2650d0fe649b3b78d910be6
Jack Mocherman <mochermanjack@gmail.com>
2016-10-03 19:48:00 -0500

    added select dropdowns to bug filter and passed priority and status up to parent component

--- src/BugFilter.jsx
+++ src/BugFilter.jsx
@@ -6,12 +6,46 @@ const ReactDOM = require('react-dom')
 const BugFilter = React.createClass({
 	render: function() {
 		return(
-			<button onClick={this.submit}>Test Filter</button>
+			<div>
+				<h3>Filter</h3>
+				Status:
+				<select value={this.state.status} onChange={this.onChangeStatus}>
+					<option value="">Any</option>
+					<option value="New">New</option>
+					<option value="Open">Open</option>
+					<option value="Closed">Closed</option>
+				</select>
+				<br/>
+				Priority:
+				<select value={this.state.priority} onChange={this.onChangePriority}>
+					<option value="">Any</option>
+					<option value="P1">P1</option>
+					<option value="P2">P2</option>
+					<option value="P3">P3</option>
+				</select>
+				<br/>
+				<button onClick={this.submit}>Apply</button>
+			</div>
 		)
 	},
 
+	getInitialState: function() {
+	    return {
+	    	status: '',
+	    	priority: ''  
+	    };
+	},
+
+	onChangeStatus: function(e) {
+		this.setState({status: e.target.value})
+	},
+
+	onChangePriority: function(e) {
+		this.setState({priority: e.target.value})
+	},
+
 	submit: function(e) {
-		this.props.submitHandler({priority: 'P1'})
+		this.props.submitHandler({priority: this.state.priority, status: this.state.status})
 	}
 })
 
