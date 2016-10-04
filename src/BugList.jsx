225621450f6f2f80af4d769ca5f4675fb3522ef8
Jack Mocherman <mochermanjack@gmail.com>
2016-10-03 21:39:50 -0500

    set location query as initFilter in render of Bug List component

--- src/BugList.jsx
+++ src/BugList.jsx
@@ -88,7 +88,7 @@ const BugList = React.createClass({
 		return(
 			<div>
 				<h1>Bug Tracker</h1>
-				<BugFilter submitHandler={this.loadData} />
+				<BugFilter submitHandler={this.loadData} initFilter={this.props.location.query} />
 				<hr />
 				<BugTable bugs={this.state.bugs} />
 				<hr />
